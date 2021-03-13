0d2e785f4f7de8b7bfbad057fc6d3db107ae6785
Donald Lee <donaldlee50@gmail.com>
2021-03-12 19:39:09 -0500

    Initial init of PointsExchange

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,13 @@
+var Escrow = artifacts.require("./Escrow.sol");
+var EscrowExchange = artifacts.require("./EscrowExchange.sol");
+
+const ether = (n) => new web3.utils.BN(web3.utils.toWei(n, 'ether'));
+
+module.exports = function(deployer, network, accounts) {
+	const _buyerAddress = accounts[0]
+	const _sellerAddress = accounts[0]
+	const _amount = ether('1');
+	const _deposit = ether('1');
+  	deployer.deploy(Escrow, _buyerAddress, _sellerAddress, _amount, _deposit);
+  	deployer.deploy(EscrowExchange);
+};

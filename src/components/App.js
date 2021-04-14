2c710e0e9f7c7a2f97e60c471eb33abd783ccf9f
Donald Lee <donaldlee50@gmail.com>
2021-04-13 21:19:41 -0400

    Added buttons and permissions to check for "Send Amount Requested"

--- src/components/App.js
+++ src/components/App.js
@@ -179,6 +179,7 @@ class App extends Component {
                   paySeller={this.paySeller}
                   refundBuyer={this.refundBuyer}
                   myContracts={this.state.contracts}
+                  account={this.state.account}
                   />
               }
             </main>
